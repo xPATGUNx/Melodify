9f2e21571ecf8c11834b1257c165f7d9b01a9345
Pascal Rutkowski <patgun@hotmail.de>
2021-06-10 16:57:15 +0200

    Implement initial generate_csv_from_single_midi_file function

--- Python/MidiToCSV.py
+++ Python/MidiToCSV.py
@@ -1,7 +1,9 @@
 import py_midicsv as pm
+import glob
 
 
-def generate_csv_from_midi():
+# TODO: refactor to take MIDI file path as parameter.
+def generate_csv_from_single_midi_file():
     csv_string = pm.midi_to_csv('../Midi Files/example.mid')
 
     with open('../CSV from MIDI/example_converted.csv', 'w') as f:
@@ -14,5 +16,18 @@ def generate_csv_from_midi():
         midi_writer.write(midi_object)
 
 
+def generate_csv_batch_from_midi_directory():
+    filecounter = 1
+    for filepath in glob.iglob('../Midi Files/*.mid'):
+        print('Converting ' + filepath[14:] + ' to CSV.')
+        csv_string = pm.midi_to_csv(filepath)
+        with open('../CSV from MIDI/CSV from Midi ' + str(filecounter) + '.csv', 'w') as f:
+            f.writelines(csv_string)
+            filecounter += 1
+    f.close()
+
+
 if __name__ == '__main__':
-    generate_csv_from_midi()
+    # generate_csv_from_single_midi_file()
+    generate_csv_batch_from_midi_directory()
+
